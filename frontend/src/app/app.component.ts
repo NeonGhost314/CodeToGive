457b3684fb0bf329f169a9d4b07488577f86c12a
yayusem <76404470+yayusem@users.noreply.github.com>
2025-11-15 15:04:11 -0500

    feat: Add HomeComponent and set as default route

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,11 +4,6 @@ import { CommonModule } from '@angular/common';
 import { ApiService } from './services/api.service';
 import { Observable, catchError, of } from 'rxjs';
 
-interface ApiResponse {
-  message: string;
-  error?: boolean;
-}
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -16,29 +11,6 @@ interface ApiResponse {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent{
   title = 'frontend';
-
-  // Message that will come from Flask (as an Observable)
-  message$!: Observable<ApiResponse>;
-
-  constructor(private apiService: ApiService, private router: Router) {}
-
-  ngOnInit() {
-    this.message$ = this.apiService.getTestData().pipe(
-      catchError(err => {
-        // Handle error if backend is not running
-        console.error(err);
-        return of({ message: "Failed to connect to Flask backend. Did you run 'flask run'?", error: true });
-      })
-    );
-  }
-
-  navigateToDonation(): void {
-    this.router.navigate(['/donation']);
-  }
-
-  navigateToVideo(): void {
-    this.router.navigate(['/video']);
-  }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { DonationComponent } from './pages/donation/donation.component';
 import { VideoComponent } from './pages/video-page/video.component';
+import { HomeComponent } from './pages/home/home.component'; // Importez le nouveau composant
 
 export const routes: Routes = [
-  {
-    path: 'donation',
-    component: DonationComponent
-  },  
-  {
-    path: 'video',
-    component: VideoComponent
-  }
+  {
+    path: '', // Route par défaut (la landing page)
+    component: HomeComponent 
+  },
+  {
+    path: 'donation',
+    component: DonationComponent
+  },  
+  {
+    path: 'video',
+    component: VideoComponent
+  }
 ];
