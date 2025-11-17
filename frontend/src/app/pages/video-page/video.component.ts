6e9ce267635cde14a63bbeee5601b4b3c3869eb4
yayusem <76404470+yayusem@users.noreply.github.com>
2025-11-17 01:20:36 -0500

    feat: video page finished

--- frontend/src/app/pages/video-page/video.component.ts
+++ frontend/src/app/pages/video-page/video.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { VideoPlayerComponent } from '../../components/video-player/video-player.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-video-page',
   standalone: true,
-  imports: [VideoPlayerComponent],
+  imports: [VideoPlayerComponent, NavbarComponent],
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.scss'],
 })
